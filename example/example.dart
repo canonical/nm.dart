98b15ebd32c9df54daf62f78a755513c821ce40a
Robert Ancell <robert.ancell@canonical.com>
2020-08-28 10:28:06 +1200

    Do enough connection to get the hostname

--- example/example.dart
+++ example/example.dart
@@ -4,7 +4,7 @@ import 'package:network_manager/network_manager.dart';
 void main() async {
   var systemBus = DBusClient.system();
   var client = NetworkManagerClient(systemBus);
-  var xml = await client.introspect();
-  print('${xml}');
+  var hostname = await client.hostname;
+  print('${hostname}');
   await systemBus.close();
 }
--- lib/src/network_manager_client.dart
+++ lib/src/network_manager_client.dart
@@ -1,9 +1,43 @@
 import 'package:dbus/dbus.dart';
 
 /// A client that connects to NetworkManager.
-class NetworkManagerClient extends DBusRemoteObject {
+class NetworkManagerClient {
+  /// The bus this client is connected to.
+  final DBusClient systemBus;
+
+  /// The root D-Bus NetworkManager object at path '/org/freedesktop'.
+  DBusRemoteObject _root;
+
+  // Objects exported on the bus.
+  final _objects = <DBusObjectPath, DBusRemoteObject>{};
+
   /// Creates a new NetworkManager client connected to the system D-Bus.
-  NetworkManagerClient(DBusClient systemBus)
-      : super(systemBus, 'org.freedesktop.NetworkManager',
-            DBusObjectPath('/org/freedesktop'));
+  NetworkManagerClient(DBusClient this.systemBus);
+
+  /// Gets the hostname
+  Future<String> get hostname async {
+    await _connect();
+    var settings =
+        _objects[DBusObjectPath('/org/freedesktop/NetworkManager/Settings')];
+    var value = await settings.getProperty(
+        'org.freedesktop.NetworkManager.Settings', 'Hostname');
+    return (value as DBusString).value;
+  }
+
+  /// Connects to the NetworkManager D-Bus objects.
+  void _connect() async {
+    // Already connected
+    if (_root != null) {
+      return;
+    }
+
+    // Find all the objects exported.
+    _root = DBusRemoteObject(systemBus, 'org.freedesktop.NetworkManager',
+        DBusObjectPath('/org/freedesktop'));
+    var objects = await _root.getManagedObjects();
+    objects.forEach((objectPath, interfaces) {
+      _objects[objectPath] = DBusRemoteObject(
+          systemBus, 'org.freedesktop.NetworkManager', objectPath);
+    });
+  }
 }
